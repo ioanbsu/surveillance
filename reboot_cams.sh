ae95fcb6ade2b10e5d52ae91c5b2ccca0800a078
Ivan Bahdanau <ibahdanau@fitbit.com>
2017-12-16 17:22:47 -0800

    reboot cams functionality added.

---
+++ reboot_cams.sh
@@ -0,0 +1,6 @@
+curl http://192.168.1.101/cgi-bin/action?cmd=reboot
+curl http://192.168.1.102/cgi-bin/action?cmd=reboot
+curl http://192.168.1.103/cgi-bin/action?cmd=reboot
+curl http://192.168.1.104/cgi-bin/action?cmd=reboot
+curl http://192.168.1.105/cgi-bin/action?cmd=reboot
+curl http://192.168.1.106/cgi-bin/action?cmd=reboot
