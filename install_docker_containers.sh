0d02c32f78c075a0b1af086576a9beb048d122a7
Ivan Bahdanau <ibahdanau@fitbit.com>
2018-09-10 09:08:02 -0700

    changing mqtt port to 8081.

--- install_docker_containers.sh
+++ install_docker_containers.sh
@@ -4,4 +4,4 @@ curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
 #buliding smartthings mqtt bridge
 sudo docker build -t smartthings-mqtt-bridge -f Dockerfile-rpi https://github.com/stjohnjohnson/smartthings-mqtt-bridge.git
 # starting smartthings mqtt bridge
-sudo docker run -d --name="mqtt-bridge" -v /opt/mqtt-bridge:/config -p 8080:8080 smartthings-mqtt-bridge
+sudo docker run -d --name="mqtt-bridge" -v /opt/mqtt-bridge:/config -p 8081:8081 smartthings-mqtt-bridge
